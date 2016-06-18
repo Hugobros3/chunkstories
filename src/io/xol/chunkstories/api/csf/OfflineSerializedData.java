3490ac741378bbbab47128e31c0590053a0b2d2d
Hugo Srevilled <hugo@xol.io>
2016-06-18 20:09:08 +0200

    ok

---
+++ src/io/xol/chunkstories/api/csf/OfflineSerializedData.java
@@ -0,0 +1,13 @@
+package io.xol.chunkstories.api.csf;
+
+//(c) 2015-2016 XolioWare Interactive
+//http://chunkstories.xyz
+//http://xol.io
+
+/**
+ * Offline serialized data is data from a .csf region or lone entity file.
+ */
+public interface OfflineSerializedData extends StreamSource, StreamTarget
+{
+
+}
