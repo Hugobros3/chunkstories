01f693d10c0452c5d2d5364ea5ea06e0966c2716
Gobrosse <Gobrosse@XOLIOWARE-L450.lan>
2017-01-12 14:50:39 +0100

    Oh you stupid .gitignore

---
+++ src/io/xol/chunkstories/api/mods/ModsManager.java
@@ -0,0 +1,41 @@
+package io.xol.chunkstories.api.mods;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import io.xol.chunkstories.api.plugin.PluginInformation;
+import io.xol.chunkstories.api.utils.IterableIterator;
+import io.xol.chunkstories.content.DefaultModsManager.ModsAssetHierarchy;
+import io.xol.chunkstories.content.mods.exceptions.NotAllModsLoadedException;
+
+//(c) 2015-2016 XolioWare Interactive
+//http://chunkstories.xyz
+//http://xol.io
+
+/**
+ * The ModsManager is responsible for loading mods and maintaining a global filesystem of assets
+ */
+public interface ModsManager
+{	
+	public void setEnabledMods(String... modsEnabled);
+
+	public void loadEnabledMods() throws NotAllModsLoadedException;
+
+	public Iterator<ModsAssetHierarchy> getAllUniqueEntries();
+
+	public Iterator<Asset> getAllUniqueFilesLocations();
+
+	public Asset getAsset(String assetName);
+
+	public AssetHierarchy getAssetInstances(String assetName);
+
+	public Iterator<Asset> getAllAssetsByExtension(String extension);
+
+	public Class<?> getClassByName(String className);
+
+	public String[] getEnabledModsString();
+
+	public Collection<Mod> getCurrentlyLoadedMods();
+
+	public IterableIterator<PluginInformation> getModsPlugins();
+}
