f3592199154a945d45949bfbcf1951b1ddf95ac8
Gobrosse <Gobrosse@Xolioware-1556.lan>
2017-03-30 15:08:59 +0200

    New version number

--- src/io/xol/chunkstories/VersionInfo.java
+++ src/io/xol/chunkstories/VersionInfo.java
@@ -6,8 +6,8 @@ package io.xol.chunkstories;
 
 public class VersionInfo
 {
-	public static String version = "2017.03.28";
-	public static int networkProtocolVersion = 31;
+	public static String version = "2017.03.30";
+	public static int networkProtocolVersion = 32;
 	
 	public static short csfFormatVersion = 0x2b;
 }
