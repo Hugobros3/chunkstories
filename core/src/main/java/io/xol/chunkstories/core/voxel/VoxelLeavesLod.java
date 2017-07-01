3e29a27aa31685558c46fe3c35456b7af6940606
Hugo Devillers (ryzen) <hugo@xol.io>
2017-07-01 15:59:54 +0200

    Implemented leaves LOD texture switch

--- core/src/main/java/io/xol/chunkstories/core/voxel/VoxelLeavesLod.java
+++ core/src/main/java/io/xol/chunkstories/core/voxel/VoxelLeavesLod.java
@@ -1,7 +1,6 @@
 package io.xol.chunkstories.core.voxel;
 
 import io.xol.chunkstories.api.voxel.Voxel;
-import io.xol.chunkstories.api.voxel.VoxelSides;
 import io.xol.chunkstories.api.voxel.VoxelType;
 import io.xol.chunkstories.api.voxel.models.ChunkRenderer;
 import io.xol.chunkstories.api.voxel.models.VoxelBakerCubic;
@@ -9,18 +8,27 @@ import io.xol.chunkstories.api.voxel.models.VoxelRenderer;
 import io.xol.chunkstories.api.voxel.models.ChunkMeshDataSubtypes.LodLevel;
 import io.xol.chunkstories.api.voxel.models.ChunkMeshDataSubtypes.ShadingType;
 import io.xol.chunkstories.api.voxel.models.ChunkRenderer.ChunkRenderContext;
+import io.xol.chunkstories.api.voxel.textures.VoxelTexture;
 import io.xol.chunkstories.api.world.VoxelContext;
 import io.xol.chunkstories.api.world.chunk.Chunk;
 import io.xol.chunkstories.voxel.VoxelsStore;
 
+//(c) 2015-2017 XolioWare Interactive
+//http://chunkstories.xyz
+//http://xol.io
+
 public class VoxelLeavesLod extends Voxel
 {
-
+	VoxelTexture baseTexture;
+	VoxelTexture opaqueTexture;
+	
 	LodedLeavesBlocksRenderer renderer = new LodedLeavesBlocksRenderer();
 	
 	public VoxelLeavesLod(VoxelType type)
 	{
 		super(type);
+		this.baseTexture = store.textures().getVoxelTextureByName(getName());
+		this.opaqueTexture = store.textures().getVoxelTextureByName(getName() + "Opaque");
 	}
 	
 	@Override
@@ -53,8 +61,8 @@ public class VoxelLeavesLod extends Voxel
 		
 		public void renderLodVersion(ChunkRenderer chunkRenderer, ChunkRenderContext bakingContext, Chunk chunk, VoxelContext voxelInformations, LodLevel lodLevel)
 		{
-			Voxel vox = voxelInformations.getVoxel();
-			int src = voxelInformations.getData();
+			//Voxel vox = voxelInformations.getVoxel();
+			//int src = voxelInformations.getData();
 			
 			int i = voxelInformations.getX() & 0x1F;
 			int k = voxelInformations.getY() & 0x1F;
@@ -65,32 +73,32 @@ public class VoxelLeavesLod extends Voxel
 			if (shallBuildWallArround(voxelInformations, 5, lodLevel))
 			{
 				if (k != 0 || bakingContext.isBottomChunkLoaded())
-					addQuadBottom(chunk, bakingContext, rawRBBF, i, k, j, vox.getVoxelTexture(src, VoxelSides.BOTTOM, voxelInformations), extraByte);
+					addQuadBottom(chunk, bakingContext, rawRBBF, i, k, j, lodLevel == LodLevel.HIGH ? baseTexture : opaqueTexture, extraByte);
 			}
 			if (shallBuildWallArround(voxelInformations, 4, lodLevel))
 			{
 				if (k != 31 || bakingContext.isTopChunkLoaded())
-					addQuadTop(chunk, bakingContext, rawRBBF, i, k, j, vox.getVoxelTexture(src, VoxelSides.TOP, voxelInformations), extraByte);
+					addQuadTop(chunk, bakingContext, rawRBBF, i, k, j, lodLevel == LodLevel.HIGH ? baseTexture : opaqueTexture, extraByte);
 			}
 			if (shallBuildWallArround(voxelInformations, 2, lodLevel))
 			{
 				if (i != 31 || bakingContext.isRightChunkLoaded())
-					addQuadRight(chunk, bakingContext, rawRBBF, i, k, j, vox.getVoxelTexture(src, VoxelSides.RIGHT, voxelInformations), extraByte);
+					addQuadRight(chunk, bakingContext, rawRBBF, i, k, j, lodLevel == LodLevel.HIGH ? baseTexture : opaqueTexture, extraByte);
 			}
 			if (shallBuildWallArround(voxelInformations, 0, lodLevel))
 			{
 				if (i != 0 || bakingContext.isLeftChunkLoaded())
-					addQuadLeft(chunk, bakingContext, rawRBBF, i, k, j, vox.getVoxelTexture(src, VoxelSides.LEFT, voxelInformations), extraByte);
+					addQuadLeft(chunk, bakingContext, rawRBBF, i, k, j, lodLevel == LodLevel.HIGH ? baseTexture : opaqueTexture, extraByte);
 			}
 			if (shallBuildWallArround(voxelInformations, 1, lodLevel))
 			{
 				if (j != 31 || bakingContext.isFrontChunkLoaded())
-					addQuadFront(chunk, bakingContext, rawRBBF, i, k, j, vox.getVoxelTexture(src, VoxelSides.FRONT, voxelInformations), extraByte);
+					addQuadFront(chunk, bakingContext, rawRBBF, i, k, j, lodLevel == LodLevel.HIGH ? baseTexture : opaqueTexture, extraByte);
 			}
 			if (shallBuildWallArround(voxelInformations, 3, lodLevel))
 			{
 				if (j != 0 || bakingContext.isBackChunkLoaded())
-					addQuadBack(chunk, bakingContext, rawRBBF, i, k, j, vox.getVoxelTexture(src, VoxelSides.BACK, voxelInformations), extraByte);
+					addQuadBack(chunk, bakingContext, rawRBBF, i, k, j, lodLevel == LodLevel.HIGH ? baseTexture : opaqueTexture, extraByte);
 			}
 		}
 	}
