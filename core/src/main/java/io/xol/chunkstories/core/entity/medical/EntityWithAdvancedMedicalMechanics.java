73cd9252eb3c28230d92e018d000ce3bdf06766e
Hugo Devillers <hugo@xol.io>
2017-10-06 09:11:58 +0200

    Removed core/ folder from this source tree, pushed it to it's own git repo.
    chunkstories-core is currently a private repo, it will go public once I clean it well enough and make it suitable for release, once that's done this will go on Github.

--- core/src/main/java/io/xol/chunkstories/core/entity/medical/EntityWithAdvancedMedicalMechanics.java
+++
@@ -1,8 +0,0 @@
-package io.xol.chunkstories.core.entity.medical;
-
-import io.xol.chunkstories.api.entity.EntityLiving;
-
-public interface EntityWithAdvancedMedicalMechanics extends EntityLiving
-{
-	public AdvancedMedicalComponent getAdvancedMedicalComponent();
-}
