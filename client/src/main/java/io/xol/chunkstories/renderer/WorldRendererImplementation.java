06a087051aa6afd2d1e8705f99caaa635f78ed82
Gobrosse <hugo@xol.io>
2018-03-04 20:00:07 +0100

    cleaner code and move gi to apply_shaddows

--- client/src/main/java/io/xol/chunkstories/renderer/WorldRendererImplementation.java
+++ client/src/main/java/io/xol/chunkstories/renderer/WorldRendererImplementation.java
@@ -6,26 +6,14 @@
 
 package io.xol.chunkstories.renderer;
 
-import static io.xol.chunkstories.api.rendering.textures.TextureFormat.DEPTH_RENDERBUFFER;
-import static io.xol.chunkstories.api.rendering.textures.TextureFormat.DEPTH_SHADOWMAP;
-import static io.xol.chunkstories.api.rendering.textures.TextureFormat.RED_8;
-import static io.xol.chunkstories.api.rendering.textures.TextureFormat.RED_8UI;
-import static io.xol.chunkstories.api.rendering.textures.TextureFormat.RGBA_8BPP;
-import static io.xol.chunkstories.api.rendering.textures.TextureFormat.RGB_8;
-import static io.xol.chunkstories.api.rendering.textures.TextureFormat.RGB_HDR;
-import static io.xol.chunkstories.api.rendering.textures.TextureFormat.RG_8;
+import static io.xol.chunkstories.api.rendering.textures.TextureFormat.*;
 
-import java.nio.ByteBuffer;
 import java.util.Iterator;
 
 import org.joml.Matrix4f;
-import org.joml.Vector4f;
-import org.joml.Vector4fc;
-import org.lwjgl.system.MemoryUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.xol.chunkstories.api.Location;
 import io.xol.chunkstories.api.client.ClientInterface;
 import io.xol.chunkstories.api.entity.Entity;
 import io.xol.chunkstories.api.entity.interfaces.EntityControllable;
@@ -40,14 +28,11 @@ import io.xol.chunkstories.api.rendering.pipeline.PipelineConfiguration.DepthTes
 import io.xol.chunkstories.api.rendering.pipeline.ShaderInterface;
 import io.xol.chunkstories.api.rendering.target.RenderTargetAttachementsConfiguration;
 import io.xol.chunkstories.api.rendering.textures.Texture2D;
-import io.xol.chunkstories.api.rendering.textures.Texture3D;
 import io.xol.chunkstories.api.rendering.textures.TextureFormat;
 import io.xol.chunkstories.api.rendering.world.WorldEffectsRenderer;
 import io.xol.chunkstories.api.rendering.world.WorldRenderer;
 import io.xol.chunkstories.api.voxel.Voxel;
-import io.xol.chunkstories.api.voxel.VoxelSides;
 import io.xol.chunkstories.api.world.WorldClient;
-import io.xol.chunkstories.api.world.cell.CellData;
 import io.xol.chunkstories.client.Client;
 import io.xol.chunkstories.client.RenderingConfig;
 import io.xol.chunkstories.gui.Ingame;
@@ -62,11 +47,9 @@ import io.xol.chunkstories.renderer.terrain.FarTerrainMeshRenderer;
 //import io.xol.chunkstories.renderer.terrain.FarTerrainNoMeshRenderer;
 import io.xol.chunkstories.renderer.terrain.SummariesArrayTexture;
 import io.xol.chunkstories.world.WorldClientCommon;
-import io.xol.chunkstories.world.chunk.CubicChunk;
 import io.xol.engine.graphics.fbo.FrameBufferObjectGL;
 import io.xol.engine.graphics.textures.CubemapGL;
 import io.xol.engine.graphics.textures.Texture2DRenderTargetGL;
-import io.xol.engine.graphics.textures.Texture3DGL;
 import io.xol.engine.graphics.textures.TexturesHandler;
 
 /** A tragically huge behemoth, held responsible of actually displaying all that mess */
@@ -401,6 +384,8 @@ public class WorldRendererImplementation implements WorldRenderer
 		entitiesRenderer.renderEntities(renderingContext);
 	}
 	
+	NearbyVoxelsVolumeTexture voxels4gi = new NearbyVoxelsVolumeTexture(this);
+	
 	/**
 	 * Uses G-Buffers data to spit out shaded solid blocks ( shadows etc )
 	 * @param sun_shadowMap 
@@ -409,9 +394,13 @@ public class WorldRendererImplementation implements WorldRenderer
 	{
 		ShaderInterface applyShadowsShader = renderingContext.useShader("shadows_apply");
 		
+		voxels4gi.update(renderingContext);
+		voxels4gi.setupForRendering(renderingContext);
+		
 		world.getGenerator().getEnvironment().setupShadowColors(renderingContext, applyShadowsShader);
 		//setupShadowColors(applyShadowsShader);
 
+		applyShadowsShader.setUniform1f("animationTimer", animationTimer);
 		applyShadowsShader.setUniform1f("overcastFactor", world.getWeather());
 		applyShadowsShader.setUniform1f("wetness", world.getGenerator().getEnvironment().getWorldWetness(renderingContext.getCamera().getCameraPosition()));
 
@@ -503,88 +492,9 @@ public class WorldRendererImplementation implements WorldRenderer
 		renderingInterface.setDepthTestMode(DepthTestMode.LESS_OR_EQUAL);
 	}
 	
-	Texture3D test = null;
-	
-	int bx,by,bz;
-	
 	@Override
 	public void blitFinalImage(RenderingInterface renderingContext, boolean hideGui)
 	{
-		if(test == null) {
-			test = new Texture3DGL(TextureFormat.RGBA_8BPP, 32, 32, 32);
-		}
-
-		final int SIZE = 128;
-		final int mod = SIZE / 32;
-
-		int offCenter = SIZE / 2;
-		
-		int chunkX = (int) ((renderingContext.getCamera().getCameraPosition().x() - offCenter) / 32);
-		int chunkY = (int) ((renderingContext.getCamera().getCameraPosition().y() - offCenter) / 32);
-		int chunkZ = (int) ((renderingContext.getCamera().getCameraPosition().z() - offCenter) / 32);
-		
-		int offsetX = chunkX % mod;
-		int offsetY = chunkY % mod;
-		int offsetZ = chunkZ % mod;
-		
-		if(bx != chunkX || by != chunkY || bz != chunkZ) {
-			
-			bx = chunkX;
-			by = chunkY;
-			bz = chunkZ;
-			ByteBuffer bb = MemoryUtil.memAlloc(4 * SIZE * SIZE * SIZE);
-			
-			byte[] empty = {0,0,0,0};
-			byte[] notempty = {-1,-1,-1,-1};
-			
-			CubicChunk zChunk = null;
-
-			Vector4f col = new Vector4f();
-			for(int a = 0; a*32 < SIZE; a++)
-				for(int b = 0; b*32 < SIZE; b++)
-					for(int c = 0; c*32 < SIZE; c++) {
-						
-						zChunk = world.getChunk(chunkX + a, chunkY + b, chunkZ + c);
-						
-						if(zChunk != null) {
-							for(int z = 0; z < 32; z++)
-								for(int y = 0; y < 32; y++) {
-	
-									int dx = (0 + a) % mod;
-									int dy = (0 + b) % mod;
-									int dz = (0 + c) % mod;
-									
-									bb.position(4 * ((dz*32 + z) * SIZE * SIZE + (dy*32 + y) * SIZE + 0 + dx*32));
-									
-									for(int x = 0; x < 32; x++) {
-										CellData cell = zChunk.peek(x, y, z);
-										Voxel voxel = cell.getVoxel();//zChunk.peekSimple(x, y, z);
-										
-										if(voxel.isAir() || voxel.getName().startsWith("glass") || !voxel.getDefinition().isSolid() && !voxel.getDefinition().isLiquid()) {
-											bb.put(empty);
-										} else {
-											col.set(voxel.getVoxelTexture(VoxelSides.TOP, cell).getColor());
-											if(col.w() < 1.0) {
-												col.mul(new Vector4f(0.1f, 0.5f, 0.1f, 1.0f));
-											}
-											
-											bb.put((byte)(int)(col.x() * 255));
-											bb.put((byte)(int)(col.y() * 255));
-											bb.put((byte)(int)(col.z() * 255));
-											bb.put(voxel.getDefinition().getEmittedLightLevel() > 0 ? (byte) 20 : (byte) 1);
-										} 
-									}
-								}
-						}
-					}
-			
-			bb.position(0);
-			bb.limit(bb.capacity());
-			test.uploadTextureData(SIZE, SIZE, SIZE, bb);
-			test.setTextureWrapping(true);
-			MemoryUtil.memFree(bb);
-		}
-		
 		//TODO mix in the reflections earlier ?
 		Texture2D bloomRendered = RenderingConfig.doBloom ? bloomRenderer.renderBloom(renderingContext) : null;
 		
@@ -598,11 +508,6 @@ public class WorldRendererImplementation implements WorldRenderer
 		renderingContext.setBlendMode(BlendMode.DISABLED);
 
 		ShaderInterface postProcess = renderingContext.useShader("postprocess");
-
-		renderingContext.bindTexture3D("currentChunk", test);
-		postProcess.setUniform1i("voxel_size", SIZE);
-		postProcess.setUniform1f("voxel_sizef", 0.0f + SIZE);
-		postProcess.setUniform3f("voxelOffset", offsetX * 32, offsetY * 32, offsetZ * 32);
 		
 		renderingContext.bindTexture2D("shadedBuffer", renderBuffers.rbShaded);
 		renderingContext.bindTexture2D("albedoBuffer", renderBuffers.rbAlbedo);
