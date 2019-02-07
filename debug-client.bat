e1338ec72df42d8a4523c0a95215f8a9850cb455
Hugo 'Gobrosse' Devillers <hugo@xol.io>
2019-02-07 10:31:45 +0100

    removing bat scripts

--- debug-client.bat
+++
@@ -1,5 +0,0 @@
-:: JAVA_HOME=C:\Program Files\Java\jdk1.8.0_65
-:: PATH=%JAVA_HOME%\bin;%PATH%
-java -version
-java -d64 -Xmx1200M -Xdebug -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Xrunjdwp:transport=dt_socket,address=7724,server=y,suspend=n -jar client/build/libs/chunkstories.jar --dir="."
-pause
