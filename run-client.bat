58217594f24cb7d3672ca12618eee6d7c9a8750c
Hugo 'Gobrosse' Devillers <hugo@xol.io>
2019-02-07 10:31:03 +0100

    removed ancient bat script

--- run-client.bat
+++
@@ -1,5 +0,0 @@
-SET JAVA_HOME=C:\Program Files\Java\jre1.8.0_65
-SET PATH=%JAVA_HOME%\bin;%PATH%
-java -version
-java -d64 -Xmx1200M -jar chunkstories.jar --dir="." --mods="C:\Users\Hugo\workspace2\Dogez-Plugin for CS\mods\dogez_content"
-pause
