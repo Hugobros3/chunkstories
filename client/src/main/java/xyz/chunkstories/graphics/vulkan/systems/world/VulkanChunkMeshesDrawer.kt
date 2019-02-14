db7844029cee2a54f0169a4947a153aa5574a4f5
Gobrosse <hugo@xol.io>
2019-02-14 11:49:24 +0100

    that doesn't belong on the gpu

--- client/src/main/java/xyz/chunkstories/graphics/vulkan/systems/world/VulkanChunkMeshesDrawer.kt
+++ client/src/main/java/xyz/chunkstories/graphics/vulkan/systems/world/VulkanChunkMeshesDrawer.kt
@@ -129,7 +129,7 @@ class VulkanCubesDrawer(pass: VulkanPass, val client: IngameClient) : VulkanDraw
         val usedData = mutableListOf<ChunkVkMeshProperty.ChunkVulkanMeshData>()
 
         //TODO pool those
-        val ssboDataTest = VulkanBuffer(backend, sizeFor4096Elements, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT, false)
+        val ssboDataTest = VulkanBuffer(backend, sizeFor4096Elements, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT, true)
 
         val ssboStuff = memAlloc(ssboDataTest.bufferSize.toInt())
         var instance = 0
