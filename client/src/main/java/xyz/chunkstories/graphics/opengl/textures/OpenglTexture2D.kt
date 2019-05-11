fdffca0c5b239162e17baae40ebbebc857481076
Gobrosse <hugo@xol.io>
2019-05-11 18:42:04 +0200

    nearest filtering by default

--- client/src/main/java/xyz/chunkstories/graphics/opengl/textures/OpenglTexture2D.kt
+++ client/src/main/java/xyz/chunkstories/graphics/opengl/textures/OpenglTexture2D.kt
@@ -14,6 +14,8 @@ class OpenglTexture2D(backend: OpenglGraphicsBackend, format: TextureFormat,
 
     init {
         glTextureStorage2D(glTexId, 1, format.glMapping.internalFormat, width, height)
+        glTextureParameteri(glTexId, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTextureParameteri(glTexId, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
     }
 
     fun upload(buffer: ByteBuffer) {
