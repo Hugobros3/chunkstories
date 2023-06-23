8a3e8367d5b60304490cc060e3ed94cd0ad31b8f
Hugo Devillers <hugo@xol.io>
2023-06-23 15:53:24 +0200

    workarround: can't have recursive subprojects

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,8 @@
 rootProject.name = "chunkstories"
 
-include("api")
+// haha what a fucking joke
+// https://stackoverflow.com/questions/18676734/gradle-recursive-subprojects
+include("api", "enklume")
+project(":enklume").projectDir = File("api/enklume")
 
 include("common", "server", "client", "converter", "launcher")
